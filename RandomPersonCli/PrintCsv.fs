fd20603a9567938741bcc5b93560f303470dcf37
Mika Sundland <mika.sundland@gmail.com>
2018-09-13 19:02:14 +0200

    Changed sb.Append to bprintf. Removed an old test playlist.

--- RandomPersonCli/PrintCsv.fs
+++ RandomPersonCli/PrintCsv.fs
@@ -2,29 +2,30 @@
 
 open System.IO
 open System.Text
+open Microsoft.FSharp.Core.Printf
 open RandomPersonLib
 open Settings
 
 let personToString (printSettings: genericPrintSettings) (person: Person) =
     let sb = StringBuilder()
-    if printSettings.FirstName                     then sb.Append(person.FirstName + ",")                        |> ignore
-    if printSettings.LastName                      then sb.Append(person.LastName + ",")                         |> ignore
-    if printSettings.Address1                      then sb.Append(person.Address1 + ",")                         |> ignore
-    if printSettings.Address2                      then sb.Append(person.Address2 + ",")                         |> ignore
-    if printSettings.PostalCode                    then sb.Append(person.PostalCode + ",")                       |> ignore
-    if printSettings.City                          then sb.Append(person.City + ",")                             |> ignore
-    if printSettings.BirthDate                     then sb.Append(person.BirthDate.ToString("yyyy-MM-dd") + ",") |> ignore
-    if printSettings.Gender                        then sb.Append(person.Gender.ToString() + ",")                |> ignore
-    if printSettings.SSN                           then sb.Append(person.SSN + ",")                              |> ignore
-    if printSettings.Nationality                   then sb.Append(person.Nationality.ToString() + ",")           |> ignore
-    if printSettings.Email                         then sb.Append(person.Email + ",")                            |> ignore
-    if printSettings.Password                      then sb.Append(person.Password + ",")                         |> ignore
-    if printSettings.MobilePhone                   then sb.Append(person.MobilePhone + ",")                      |> ignore
-    if printSettings.HomePhone                     then sb.Append(person.HomePhone + ",")                        |> ignore
-    if printSettings.CountryNameEnglish            then sb.Append(person.CountryNameEnglish + ",")               |> ignore
-    if printSettings.CountryNameNative             then sb.Append(person.CountryNameNative + ",")                |> ignore
-    if printSettings.CountryNameNativeAlternative1 then sb.Append(person.CountryNameNativeAlternative1 + ",")    |> ignore
-    if printSettings.CountryNameNativeAlternative2 then sb.Append(person.CountryNameNativeAlternative2 + ",")    |> ignore
+    if printSettings.FirstName                     then bprintf sb "%s" (person.FirstName + ",")                        |> ignore
+    if printSettings.LastName                      then bprintf sb "%s" (person.LastName + ",")                         |> ignore
+    if printSettings.Address1                      then bprintf sb "%s" (person.Address1 + ",")                         |> ignore
+    if printSettings.Address2                      then bprintf sb "%s" (person.Address2 + ",")                         |> ignore
+    if printSettings.PostalCode                    then bprintf sb "%s" (person.PostalCode + ",")                       |> ignore
+    if printSettings.City                          then bprintf sb "%s" (person.City + ",")                             |> ignore
+    if printSettings.BirthDate                     then bprintf sb "%s" (person.BirthDate.ToString("yyyy-MM-dd") + ",") |> ignore
+    if printSettings.Gender                        then bprintf sb "%s" (person.Gender.ToString() + ",")                |> ignore
+    if printSettings.SSN                           then bprintf sb "%s" (person.SSN + ",")                              |> ignore
+    if printSettings.Nationality                   then bprintf sb "%s" (person.Nationality.ToString() + ",")           |> ignore
+    if printSettings.Email                         then bprintf sb "%s" (person.Email + ",")                            |> ignore
+    if printSettings.Password                      then bprintf sb "%s" (person.Password + ",")                         |> ignore
+    if printSettings.MobilePhone                   then bprintf sb "%s" (person.MobilePhone + ",")                      |> ignore
+    if printSettings.HomePhone                     then bprintf sb "%s" (person.HomePhone + ",")                        |> ignore
+    if printSettings.CountryNameEnglish            then bprintf sb "%s" (person.CountryNameEnglish + ",")               |> ignore
+    if printSettings.CountryNameNative             then bprintf sb "%s" (person.CountryNameNative + ",")                |> ignore
+    if printSettings.CountryNameNativeAlternative1 then bprintf sb "%s" (person.CountryNameNativeAlternative1 + ",")    |> ignore
+    if printSettings.CountryNameNativeAlternative2 then bprintf sb "%s" (person.CountryNameNativeAlternative2 + ",")    |> ignore
 
     sb.Remove(sb.Length - 1, 1) |> ignore
 
@@ -32,24 +33,24 @@ let personToString (printSettings: genericPrintSettings) (person: Person) =
 
 let createHeader (printSettings: genericPrintSettings) =
     let sb = StringBuilder()
-    if printSettings.FirstName                     then sb.Append("FirstName,")                     |> ignore
-    if printSettings.LastName                      then sb.Append("LastName,")                      |> ignore
-    if printSettings.Address1                      then sb.Append("Address1,")                      |> ignore
-    if printSettings.Address2                      then sb.Append("Address2,")                      |> ignore
-    if printSettings.PostalCode                    then sb.Append("PostalCode,")                    |> ignore
-    if printSettings.City                          then sb.Append("City,")                          |> ignore
-    if printSettings.BirthDate                     then sb.Append("BirthDate,")                     |> ignore
-    if printSettings.Gender                        then sb.Append("Gender,")                        |> ignore
-    if printSettings.SSN                           then sb.Append("SSN,")                           |> ignore
-    if printSettings.Nationality                   then sb.Append("Nationality,")                   |> ignore
-    if printSettings.Email                         then sb.Append("Email,")                         |> ignore
-    if printSettings.Password                      then sb.Append("Password,")                      |> ignore
-    if printSettings.MobilePhone                   then sb.Append("MobilePhone,")                   |> ignore
-    if printSettings.HomePhone                     then sb.Append("HomePhone,")                     |> ignore
-    if printSettings.CountryNameEnglish            then sb.Append("CountryNameEnglish,")            |> ignore
-    if printSettings.CountryNameNative             then sb.Append("CountryNameNative,")             |> ignore
-    if printSettings.CountryNameNativeAlternative1 then sb.Append("CountryNameNativeAlternative1,") |> ignore
-    if printSettings.CountryNameNativeAlternative2 then sb.Append("CountryNameNativeAlternative2,") |> ignore
+    if printSettings.FirstName                     then bprintf sb "%s" ("FirstName,")                     |> ignore
+    if printSettings.LastName                      then bprintf sb "%s" ("LastName,")                      |> ignore
+    if printSettings.Address1                      then bprintf sb "%s" ("Address1,")                      |> ignore
+    if printSettings.Address2                      then bprintf sb "%s" ("Address2,")                      |> ignore
+    if printSettings.PostalCode                    then bprintf sb "%s" ("PostalCode,")                    |> ignore
+    if printSettings.City                          then bprintf sb "%s" ("City,")                          |> ignore
+    if printSettings.BirthDate                     then bprintf sb "%s" ("BirthDate,")                     |> ignore
+    if printSettings.Gender                        then bprintf sb "%s" ("Gender,")                        |> ignore
+    if printSettings.SSN                           then bprintf sb "%s" ("SSN,")                           |> ignore
+    if printSettings.Nationality                   then bprintf sb "%s" ("Nationality,")                   |> ignore
+    if printSettings.Email                         then bprintf sb "%s" ("Email,")                         |> ignore
+    if printSettings.Password                      then bprintf sb "%s" ("Password,")                      |> ignore
+    if printSettings.MobilePhone                   then bprintf sb "%s" ("MobilePhone,")                   |> ignore
+    if printSettings.HomePhone                     then bprintf sb "%s" ("HomePhone,")                     |> ignore
+    if printSettings.CountryNameEnglish            then bprintf sb "%s" ("CountryNameEnglish,")            |> ignore
+    if printSettings.CountryNameNative             then bprintf sb "%s" ("CountryNameNative,")             |> ignore
+    if printSettings.CountryNameNativeAlternative1 then bprintf sb "%s" ("CountryNameNativeAlternative1,") |> ignore
+    if printSettings.CountryNameNativeAlternative2 then bprintf sb "%s" ("CountryNameNativeAlternative2,") |> ignore
 
     match sb.Length with
     | length when 1 < length -> sb.Remove(sb.Length - 1, 1) |> ignore
